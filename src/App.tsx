1abd277ab62d13c1171bd9ba3b3efcec028acf02
ryanteozw <ryanteozw@gmail.com>
2025-09-21 14:14:30 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Window } from './components/Window'
+
+function App() {
+    return (
+        <>
+            <h1 className="font-black">Test</h1>
+            <Window>
+                <h2 className="font-bold">Hello, World!</h2>
+            </Window>
+        </>
+    )
+}
+
+export default App
