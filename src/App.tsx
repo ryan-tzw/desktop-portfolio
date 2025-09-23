78b8d50a1aba05857bd6570e4d8f7c49c05b2e2e
ryanteozw <ryanteozw@gmail.com>
2025-09-23 09:27:23 +0800

    switch to dndkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { Window } from './components/Window'
+import './App.css'
+import { Taskbar } from './components/Taskbar'
+import { DndContext } from '@dnd-kit/core'
+import { Draggable } from './components/DraggableTest'
 
 function App() {
     return (
         <>
-            <h1 className="font-black">Test</h1>
-            <Window>
-                <h2 className="font-bold">Hello, World!</h2>
-            </Window>
+            {/* <Taskbar /> */}
+            {/* <div className="h-dvh content-center">
+                <h1 className="text-center font-black">Test</h1>
+            </div> */}
+            <DndContext>
+                <Window title="My First Window">
+                    <h2 className="font-manrope">Hello, World!</h2>
+                </Window>
+                <Draggable>Drag me!</Draggable>
+            </DndContext>
         </>
     )
 }
---
+++ src/components/Taskbar.tsx
@@ -0,0 +1,7 @@
+export function Taskbar() {
+    return (
+        <>
+            <div className="fixed h-10 w-full bg-[rgba(255,255,255,0.8)] shadow-sm"></div>
+        </>
+    )
+}
