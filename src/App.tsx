9050890f4262be62e14bec9f2575658b2c6ffda4
ryanteozw <ryanteozw@gmail.com>
2026-01-28 11:52:59 +0800

    direct link to project or other window

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css'
+import { useEffect } from 'react'
 import { Taskbar } from './components/Taskbar'
 import { Background } from './system/background'
 import { Launcher } from './system/layout/Launcher'
 import { WindowManager } from './system/windows/WindowManager'
+import { useWindowStore } from './stores/windows.store'
 
 function App() {
+    const open = useWindowStore((state) => state.open)
+
+    // on mount check for ?window= or ?project= in URL to open specific window
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search)
+        const windowId = params.get('window')
+        const projectId = params.get('project')
+
+        if (projectId) {
+            open('project', { projectId })
+        } else if (windowId) {
+            open(windowId)
+        }
+    }, [open])
+
     return (
         <>
             <Taskbar />
